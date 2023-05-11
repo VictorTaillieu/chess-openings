18a75c4d71e30ecafe1e6844f1c3a8f611e7af1c
Vic8t <vic8.taillieu@gmail.com>
2023-05-11 20:53:43 +0200

    Sort by statistics

--- app.py
+++ app.py
@@ -26,6 +26,20 @@ app.layout = html.Div([
         value=["white", "black"],
         multi=True
     ),
+    dcc.Dropdown(
+        id='sort',
+        options=[
+            {"label": "Number of masters games played", "value": "total_masters"},
+            {"label": "Number of lichess games played", "value": "total_lichess"},
+            {"label": "Masters white success rate", "value": "white_masters"},
+            {"label": "Masters black success rate", "value": "black_masters"},
+            {"label": "Masters draw rate", "value": "draws_masters"},
+            {"label": "Lichess white success rate", "value": "white_lichess"},
+            {"label": "Lichess black success rate", "value": "black_lichess"},
+            {"label": "Lichess draw rate", "value": "draws_lichess"}
+        ],
+        value="total_masters"
+    ),
     dash_table.DataTable(
         id="table",
         fixed_rows={"headers": True},
@@ -37,9 +51,10 @@ app.layout = html.Div([
 @app.callback(
     Output("table", "data"),
     Input("open-var", "value"),
-    Input("color", "value")
+    Input("color", "value"),
+    Input("sort", "value"),
 )
-def update_table(open_var, color):
+def update_table(open_var, color, sort, selector):
     dff = df.copy()
 
     # Filter openings or variations
@@ -51,6 +66,9 @@ def update_table(open_var, color):
 
     # Filter colors
     dff = dff[dff.color.isin(color)]
+
+    # Sort
+    dff.sort_values(sort, ascending=False, inplace=True)
 
     return dff.to_dict("records")
 
